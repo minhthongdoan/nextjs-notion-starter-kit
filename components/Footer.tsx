c650a54488bf5ab1942a9e0d7bea9c31d859fd85
minhthongdoan <bubuthaman00@gmail.com>
2021-06-22 16:00:44 +0700

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -25,8 +25,8 @@ export const Footer: React.FC<{
   }, [])
 
   return (
-    <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2021 {config.author}</div>
+   // <footer className={styles.footer}>
+     // <div className={styles.copyright}>Copyright 2021 {config.author}</div>
 
       {hasMounted ? (
         <div className={styles.settings}>

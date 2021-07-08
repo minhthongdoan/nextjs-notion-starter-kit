f3a35675ab8ced2706f1cd8f0f41a59e64885459
Minh Thong <bubuthaman00@gmail.com>
2021-07-08 20:38:22 +0700

    remove fathom

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -93,16 +93,3 @@ export const Footer: React.FC<{
   )
 }
 
-
-<><script>
-  (function(f, a, t, h, o, m){a[h] = a[h] || function () {
-    (a[h].q = a[h].q || []).push(arguments)
-  } };
-  o=f.createElement('script'),
-  m=f.getElementsByTagName('script')[0];
-  o.async=1; o.src=t; o.id='fathom-script';
-  m.parentNode.insertBefore(o,m)
-  })(document, window, '//web-fathom.herokuapp.com/tracker.js', 'fathom');
-  fathom('set', 'siteId', 'CAXFD');
-  fathom('trackPageview');
-</script>< /></>!-- / Fathom -->

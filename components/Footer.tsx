1ed7fe8e551c69bbbd13b23e4ffb5b2787b29e24
Minh Thong <bubuthaman00@gmail.com>
2021-07-08 20:07:54 +0700

    fathom

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -92,3 +92,17 @@ export const Footer: React.FC<{
     </footer>
   )
 }
+
+
+<><script>
+  (function(f, a, t, h, o, m){a[h] = a[h] || function () {
+    (a[h].q = a[h].q || []).push(arguments)
+  } };
+  o=f.createElement('script'),
+  m=f.getElementsByTagName('script')[0];
+  o.async=1; o.src=t; o.id='fathom-script';
+  m.parentNode.insertBefore(o,m)
+  })(document, window, '//web-fathom.herokuapp.com/tracker.js', 'fathom');
+  fathom('set', 'siteId', 'CAXFD');
+  fathom('trackPageview');
+</script>< /></>!-- / Fathom -->

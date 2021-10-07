976bc3d76535989a860069e2169ee88304aba751
Minh Thong <bubuthaman00@gmail.com>
2021-10-07 23:21:22 +0700

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,7 +26,7 @@ export const Footer: React.FC<{
 
   return (
    <footer className={styles.footer}>
-     <div className={styles.copyright}>Copyright © 2021 {config.author}</div>
+     <div className={styles.copyright}>
      Copyright 2021 {config.author}
         <br />
         Template by{' '}
